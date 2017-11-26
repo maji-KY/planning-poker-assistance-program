dfedb73c9cbd447dead9849e5d1a5f9869fc6be1
maji-KY <maji-ky@neco-labo.com>
2017-11-27 00:56:36 +0900

    add cors

--- functions/src/main/organization.ts
+++ functions/src/main/organization.ts
@@ -1,24 +1,29 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as cors from "cors";
+
+const corsFilter = cors({"origin": true});
 
 export const createOrganization = functions.https.onRequest(async (req, res) => {
-  const { userId, organizationName } = req.body;
+  corsFilter(req, res, await (async () => {
+    const { userId, organizationName } = req.body;
 
-  if (userId && organizationName) {
-    const createdOrganizationId = (+new Date).toString(36);
-    const fs = admin.firestore();
-    await fs.collection("organizations").doc(createdOrganizationId).set({
-      "name": organizationName
-    });
-    const uo = await fs.collection("UserOrganizations").doc(userId).get();
-    const currentData = uo.exists ? uo.data() : {"organizationIds": []};
-    const currentOrganizationIds: string[] = currentData.organizationIds;
-    await fs.collection("UserOrganizations").doc(userId).set({
-      "organizationIds": currentOrganizationIds.concat(createdOrganizationId)
-    });
+    if (userId && organizationName) {
+      const createdOrganizationId = (+new Date).toString(36);
+      const fs = admin.firestore();
+      await fs.collection("organizations").doc(createdOrganizationId).set({
+        "name": organizationName
+      });
+      const uo = await fs.collection("UserOrganizations").doc(userId).get();
+      const currentData = uo.exists ? uo.data() : {"organizationIds": []};
+      const currentOrganizationIds: string[] = currentData.organizationIds;
+      await fs.collection("UserOrganizations").doc(userId).set({
+        "organizationIds": currentOrganizationIds.concat(createdOrganizationId)
+      });
 
-    res.status(201).send("created");
-  } else {
-    res.status(400).send("userId and organizationName required");
-  }
+      res.status(201).send("created");
+    } else {
+      res.status(400).send("userId and organizationName required");
+    }
+  }));
 });
